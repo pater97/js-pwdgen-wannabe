ceaa516a918209850cdfa8931125dfad6f776414
pater97 <giuseppepater997@gmail.com>
2021-10-08 13:39:47 +0200

    esercizio completato con risultato centrato

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // domande
 alert(`creiamo una password insicura`)
 const user_name = prompt(`come ti chiami?`);
-const last_name = prompt(`qualè il tuo cognome`);
-const best_color = prompt(`quel'è il tuo colore preferito?`);
+const last_name = prompt(`qual è il tuo cognome`);
+const best_color = prompt(`qual è il tuo colore preferito?`);
 const number_random = (`21`)
 // verifica
 console.log(`Ecco la tua password` + user_name + last_name + best_color);
