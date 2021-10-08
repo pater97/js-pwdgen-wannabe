675026c3df6f5c7bb78f2e7b7e1a382b594dace0
pater97 <giuseppepater997@gmail.com>
2021-10-08 13:36:56 +0200

    esercizio completato con risultato centrato

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ alert(`creiamo una password insicura`)
 const user_name = prompt(`come ti chiami?`);
 const last_name = prompt(`qualè il tuo cognome`);
 const best_color = prompt(`quel'è il tuo colore preferito?`);
+const number_random = (`21`)
 // verifica
 console.log(`Ecco la tua password` + user_name + last_name + best_color);
 // inserimento in html
-document.getElementById(`password`).innerHTML = `Ecco la tua password insicura: ` + user_name + last_name + best_color
+document.getElementById(`password`).innerHTML = `Ecco la tua password insicura: ` + user_name + last_name + best_color + number_random
